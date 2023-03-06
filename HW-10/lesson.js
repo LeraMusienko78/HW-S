a0977b0008074863cf9f6aa5c556c2fe76807aea
VALERIIA MUSIENKO <98159073+Valeria378@users.noreply.github.com>
2023-03-06 15:05:08 +0200

    added task-6 (dop)

--- HW-10/lesson.js
+++ HW-10/lesson.js
@@ -196,6 +196,7 @@
 //         ********** HOMWORK ***********
 
 const allInOne = [];
+
 console.log("[ALL IN ONE]", allInOne);
 
 const auto = 
@@ -297,6 +298,7 @@ console.log("[COST OF ALL AUTO]", " *** " + result + " $");
 //* 3) Concat arr truck and sedan in allInOne
 
 allInOne.push(...auto.truck, ...auto.sedan);
+console.log(allInOne.length);
 
 //* 4) Get from allInOne only truck , only sendan
 
@@ -306,6 +308,30 @@ console.log("[GET TRUCK]", getTruck);
 //  4.2)
 const getSedan = allInOne.filter(typeSedan => typeSedan.type === "SEDAN");
 console.log("[GET SEDAN]", getSedan);
+
+//* 5) Find elem by index '1' in allInOne and next + prev => [prev , 1 ,next]
+
+console.log("[ ELEMENT AT INDEX '1' ]" ,allInOne[1]);
+
+const newArr = allInOne.slice(0,3);
+console.log("[NEW ARR]", newArr);
+
+// 6) Вывести только названия авто:
+
+const arrLabel = allInOne.forEach( element => console.log(element.label));
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
